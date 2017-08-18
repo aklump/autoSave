e4acebccfdb20fd204280f4e41f1df6cabdfe990
Aaron Klump <sourcecode@intheloftstudios.com>
2017-08-17 17:24:00 -0700

    add support for checkbox.

--- jquery.autoSave.js
+++ jquery.autoSave.js
@@ -1,24 +1,31 @@
 /*
  jQuery autoSave v1.0.0 - 2013-04-05
+ (c) 2017 Aaron Klump
  (c) 2013 Yang Zhao - geniuscarrier.com
  license: http://www.opensource.org/licenses/mit-license.php
  */
-(function($) {
-    $.fn.autoSave = function(callback, ms) {
-        return this.each(function() {
-            var timer = 0, 
-                $this = $(this),
-                delay = ms || 1000;
-            $this.keyup(function() {
-                clearTimeout(timer);
-                var $context = $this.val();
-                if(localStorage) {
-                    localStorage.setItem("autoSave", $context);
-                }
-                timer = setTimeout(function() {
-                    callback();
-                }, delay);
-            });
-        });
-    };
+(function ($) {
+  $.fn.autoSave = function (callback, ms) {
+    return this.each(function () {
+      var timer = 0,
+          $this = $(this),
+          delay = ms || 1000,
+          type  = $this.attr('type');
+      switch (type) {
+        case 'checkbox':
+          $this.on('click', callback);
+          break;
+        default:
+          $this.keyup(function () {
+            clearTimeout(timer);
+            var $context = $this.val();
+            if (localStorage) {
+              localStorage.setItem("autoSave", $context);
+            }
+            timer = setTimeout(callback, delay);
+          });
+          break;
+      }
+    });
+  };
 })(jQuery);
